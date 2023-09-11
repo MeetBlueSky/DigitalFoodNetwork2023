e4d2297c2b37878579bfe0defa703b1bd7775e27
Gürkan Arslan <myst-ga@hotmail.com>
2023-09-11 12:17:54 +0300

    DB değişiklikleri

--- NewVersion/DFN2023.Entities/DTO/SliderDT.cs
+++ NewVersion/DFN2023.Entities/DTO/SliderDT.cs
@@ -10,9 +10,11 @@ namespace DFN2023.Entities.DTO
         public string Image1 { get; set; }
         public string Image2 { get; set; }
         public string Name { get; set; }
-        public string Link { get; set; }
-        public string Target { get; set; }
-        public int Type { get; set; }
+        public string? Link { get; set; }
+        public string? Target { get; set; }
+        public int? Type { get; set; }
+        public string? Header { get; set; }
+        public string? Text { get; set; }
         public int RowNum { get; set; }
         public int Status { get; set; }
         public int LangId { get; set; }
--- NewVersion/DFN2023.Infrastructure/Migrations/Sql/20230911091630_11092023.Designer.cs
+++ NewVersion/DFN2023.Infrastructure/Migrations/Sql/20230911091630_11092023.Designer.cs
@@ -4,6 +4,7 @@ using DFN2023.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DFN2023.Infrastructure.Migrations.Sql
 {
     [DbContext(typeof(SqlContext))]
-    partial class SqlContextModelSnapshot : ModelSnapshot
+    [Migration("20230911091630_11092023")]
+    partial class _11092023
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
